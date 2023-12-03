bdd6b019a8610321b9ed261e7bd9dcc59aaec767
rafailvv <rafail.02@mail.ru>
2023-12-03 23:55:12 +0300

    Add: final_report.pdf

--- benchmark/evaluate.py
+++ benchmark/evaluate.py
@@ -1,54 +1,58 @@
+"""Evaluating movie recommendation model"""
+# Importing necessary libraries
 import time
-
 import pandas as pd
+import pickle
 import numpy as np
 import torch
 from torch import nn
 from torch.nn.functional import mse_loss
 from torch.utils.data import DataLoader
-
 from benchmark.data.dataset import MovieDataset
-import pickle
 
-
+# Definition of the recommender neural network class
 class RecommenderNet(nn.Module):
+    """Model structure"""
     def __init__(
         self,
-        num_zip_codes,
-        num_release_years,
-        num_occupations,
-        num_genres,
-        embedding_size,
+        number_zip_codes,
+        number_release_years,
+        number_occupations,
+        number_genres,
+        embedding_size_value,
     ):
         super(RecommenderNet, self).__init__()
-        # Embeddings
-        self.zip_code_embedding = nn.Embedding(num_zip_codes, embedding_size)
-        self.release_year_embedding = nn.Embedding(num_release_years, embedding_size)
-        self.occupation_embedding = nn.Embedding(num_occupations, embedding_size)
+        # Creating embeddings for zip codes, release years, and occupations
+        self.zip_code_embedding = nn.Embedding(number_zip_codes, embedding_size_value)
+        self.release_year_embedding = nn.Embedding(number_release_years, embedding_size_value)
+        self.occupation_embedding = nn.Embedding(number_occupations, embedding_size_value)
 
-        # Linear layers for age and gender
-        self.age_lin = nn.Linear(1, embedding_size)
-        self.gender_lin = nn.Linear(2, embedding_size)
+        # Linear layers to process age and gender data
+        self.age_lin = nn.Linear(1, embedding_size_value)
+        self.gender_lin = nn.Linear(2, embedding_size_value)
 
-        # Fully connected layers
-        self.fc1 = nn.Linear(embedding_size * 5 + num_genres, 128)
+        # Fully connected layers for learning complex relationships
+        self.fc1 = nn.Linear(embedding_size_value * 5 + number_genres, 128)
         self.bn1 = nn.BatchNorm1d(128)
         self.fc2 = nn.Linear(128, 64)
         self.bn2 = nn.BatchNorm1d(64)
         self.fc3 = nn.Linear(64, 1)
 
-        # Dropout layer
+        # Dropout layer to reduce overfitting
         self.dropout = nn.Dropout(0.2)
 
+    # Forward pass of the neural network
     def forward(
         self, zip_codes, release_years, ages, occupations, genders, genre_features
     ):
+        # Generating embeddings for each feature
         zip_code_embedding = self.zip_code_embedding(zip_codes)
         release_year_embedding = self.release_year_embedding(release_years)
         occupation_embedding = self.occupation_embedding(occupations)
         age_embedding = self.age_lin(ages.unsqueeze(1))
         gender_embedding = self.gender_lin(genders)
 
+        # Concatenating features and passing through neural network
         x = torch.cat(
             [
                 zip_code_embedding,
@@ -60,6 +64,7 @@ class RecommenderNet(nn.Module):
             ],
             dim=1,
         )
+        # Activation functions and batch normalization
         x = nn.ReLU()(self.bn1(self.fc1(x)))
         x = self.dropout(x)
         x = nn.ReLU()(self.bn2(self.fc2(x)))
@@ -67,6 +72,7 @@ class RecommenderNet(nn.Module):
         return x.squeeze()
 
 
+# Loading and preprocessing the dataset
 df = pd.read_csv("data/preprocessed.csv")
 all_genres = [
     "unknown",
@@ -90,6 +96,7 @@ all_genres = [
     "Western",
 ]
 
+# Loading encoders for occupation, zip code, and release year
 with open("../models/occupation_encoder.pkl", "rb") as file:
     occupation_encoder = pickle.load(file)
 with open("../models/zip_code_encoder.pkl", "rb") as file:
@@ -97,27 +104,33 @@ with open("../models/zip_code_encoder.pkl", "rb") as file:
 with open("../models/release_year_encoder.pkl", "rb") as file:
     release_year_encoder = pickle.load(file)
 
-# occupation_encoder = LabelEncoder()
-# zip_code_encoder = LabelEncoder()
-# release_year_encoder = LabelEncoder()
-# df['zip_code'] = zip_code_encoder.fit_transform(df['zip_code'])
-# df['release_year'] = release_year_encoder.fit_transform(df['release_year'])
-# df['occupation'] = occupation_encoder.fit_transform(df['occupation'])
-
-embedding_size = 50
-batch_size = 32
+# Setting parameters for the model
+EMBEDDING_SIZE = 50
+BATCH_SIZE = 32
 num_zip_codes = df["zip_code"].nunique()
 num_release_years = df["release_year"].nunique()
 num_movies = df["film_id"].nunique()
 num_occupations = df["occupation"].nunique()
-num_genres = len(all_genres)
+NUM_GENRES = len(all_genres)
+
+# Creating the model and loading saved weights
 model = RecommenderNet(
-    num_zip_codes, num_release_years, num_occupations, num_genres, embedding_size
+    num_zip_codes, num_release_years, num_occupations, NUM_GENRES, EMBEDDING_SIZE
 )
 model.load_state_dict(torch.load("../models/model.pth"))
 
 
 def recommend_movies(model, age, gender, occupation, zip_code, num_recommendations=5):
+    """
+    Function to recommend movies based on user inputs
+    :param model: reccomendation model
+    :param age: age of the user
+    :param gender: user gender
+    :param occupation: user occupation
+    :param zip_code: user zip_code
+    :param num_recommendations: number of top films reccomendaition
+    :return:
+    """
     # Convert inputs using encoders and normalization
     encoded_occupation = occupation_encoder.transform([occupation])[0]
     encoded_zip_code = zip_code_encoder.transform([zip_code])[0]
@@ -143,22 +156,22 @@ def recommend_movies(model, age, gender, occupation, zip_code, num_recommendatio
     model.eval()
     predictions = np.zeros(num_movies)
     with torch.no_grad():
-        for i in range(0, num_movies, batch_size):
-            if i + batch_size > num_movies:
-                i = num_movies - batch_size
+        for i in range(0, num_movies, BATCH_SIZE):
+            if i + BATCH_SIZE > num_movies:
+                i = num_movies - BATCH_SIZE
             genre_features = torch.tensor(
                 df.loc[:, list(all_genres)].values, dtype=torch.float32
             )
             release_years = torch.tensor(df["release_year"].tolist())
             batch_predictions = model(
-                zip_codes_tensor[i : i + batch_size],
-                release_years[i : i + batch_size],
-                ages_tensor[i : i + batch_size],
-                occupations_tensor[i : i + batch_size],
-                genders_tensor[i : i + batch_size],
-                genre_features[i : i + batch_size],
+                zip_codes_tensor[i : i + BATCH_SIZE],
+                release_years[i : i + BATCH_SIZE],
+                ages_tensor[i : i + BATCH_SIZE],
+                occupations_tensor[i : i + BATCH_SIZE],
+                genders_tensor[i : i + BATCH_SIZE],
+                genre_features[i : i + BATCH_SIZE],
             )
-            predictions[i : i + batch_size] = batch_predictions.numpy()
+            predictions[i : i + BATCH_SIZE] = batch_predictions.numpy()
 
     # Sort by predicted rating
     sorted_indices = np.argsort(predictions)[::-1]
@@ -178,6 +191,12 @@ def recommend_movies(model, age, gender, occupation, zip_code, num_recommendatio
 
 
 def evaluate_model(model, test_loader):
+    """
+    Calculating the Root Mean Square Error for the model
+    :param model: reccomendation model
+    :param test_loader: test loader of the dataset
+    :return: RMSE value
+    """
     model.eval()
     mse = 0
     with torch.no_grad():
@@ -200,8 +219,9 @@ def evaluate_model(model, test_loader):
     return np.sqrt(mse)
 
 
-# Example user details
+# Interactive user interface for the recommender system
 while True:
+    # Menu for user interaction
     menu = int(
         input(
             """
@@ -215,7 +235,14 @@ Enter 1, 2, 3 or 4: """
         )
     )
 
-    if menu == 2:
+    if menu == 1:
+        # Handling manual input of user data
+        age = int(input("Age: "))
+        gender = input("Gender M (male) or F (female): ")
+        occupation = input("Occupation: ")
+        zip_code = input("Zip code: ")
+    elif menu == 2:
+        # Handling option to enter existing user's ID
         user_df = pd.read_csv("../data/interim/user.csv")
         user_id = int(
             input(f"Enter user_id (from 1 to {len(user_df['user_id'].tolist())}):")
@@ -234,13 +261,9 @@ Enter 1, 2, 3 or 4: """
         else:
             print(f"User ID {user_id} not found in the dataset.")
             continue
-    elif menu == 1:
-        age = int(input("Age: "))
-        gender = input("Gender M (male) or F (female): ")
-        occupation = input("Occupation: ")
-        zip_code = input("Zip code: ")
     elif menu == 3:
-        test_loader = DataLoader(MovieDataset(df), batch_size=batch_size)
+        # Calculating RMSE for the entire dataset
+        test_loader = DataLoader(MovieDataset(df), batch_size=BATCH_SIZE)
         rmse = evaluate_model(model, test_loader)
         print(f"RMSE on test set: {rmse}")
         time.sleep(1)
